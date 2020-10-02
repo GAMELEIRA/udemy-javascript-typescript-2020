369834132885ef3ec8a47ce664ca70f9df5b4660
GAMELEIRA <gameleira270499@gmail.com>
2020-10-02 00:48:40 -0300

    Adicionado JS-BASICO o modulo desafio-console.js que têm como objetivo exibir uma mensagem no console

---
+++ JS-BASICO/desafio-console.js
@@ -0,0 +1,8 @@
+/**
+ * AUTOR: GABRIEL GAMELEIRA DOS SANTOS
+ * DATA: 02/10/2020
+ * HORA: 00:14
+ * FINALIDADE DO MODULO: UTILIZAR O CONSOLE.LOG 
+ */
+
+console.log(`Meu nome é "NOME". Estou aprendendo JavaScript ás ${(new Date).getHours()}.`);
