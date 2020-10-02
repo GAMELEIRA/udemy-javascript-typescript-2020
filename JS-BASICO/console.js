d6afbc1fe740d7c306090ca4fc2ffbab3d8db23c
GAMELEIRA <gameleira270499@gmail.com>
2020-10-02 00:47:06 -0300

    Adicionado SECAO-01 o modulo console.js que têm como objetivo exibir mensagens de console na tela

---
+++ JS-BASICO/console.js
@@ -0,0 +1,14 @@
+/**
+ * AUTOR: GABRIEL GAMELEIRA DOS SANTOS
+ * DATA: 02/10/2020
+ * HORA: 00:14
+ * FINALIDADE DO MODULO: UTILIZAR O CONSOLE.LOG 
+ */
+
+console.log("Gabriel Gameleira dos Santos");
+
+console.log('Gabriel Gameleira dos Santos');
+
+console.log(`'Gabriel' "Gameleira"`);
+
+console.log(123456, 15.85, "Gabriel Gameleira");
