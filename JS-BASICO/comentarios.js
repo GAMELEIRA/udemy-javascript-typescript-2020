944b70122ff46cc8f7f42014564289883cb61f4f
GAMELEIRA <gameleira270499@gmail.com>
2020-10-02 01:02:04 -0300

    Adicionado JS-BASICO o modulo comentario.js que têm como objetivo exibir mostrar a estrutura de comentários em js

---
+++ JS-BASICO/comentarios.js
@@ -0,0 +1,17 @@
+/**
+ * AUTOR: GABRIEL GAMELEIRA DOS SANTOS
+ * DATA: 02/10/2020
+ * HORA: 00:50
+ * FINALIDADE DO MODULO: CRIAR COMENTARIOS
+ */
+
+//E ESCREVER UM COMENTARIO
+console.log("Olá mundo!"); //E ESCREVER UM COMENTARIO
+
+console.log("Olá mundo!");
+
+console.log("Olá mundo!");
+
+console.log("Olá mundo!");
+
+console.log("Olá mundo!");
