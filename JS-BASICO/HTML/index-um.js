fc179e4376f38088b04545a10e6a8943a71839de
GAMELEIRA <gameleira270499@gmail.com>
2020-10-18 22:46:47 -0300

    Atualizando projeto com o conteúdo de TSC.

---
+++ JS-BASICO/HTML/index-um.js
@@ -0,0 +1,5 @@
+const numeroUm = Number(prompt("Informe um número: "));
+
+const numeroDois = Number(prompt("Informe outro número: "));
+
+alert("A soma dos números é ".concat(numeroUm + numeroDois));
---
+++ JS-BASICO/desafio.js
@@ -0,0 +1,19 @@
+let a = 'A';
+
+let b = 'B';
+
+let c = 'C';
+
+let temporario = '';
+
+temporario = a;
+
+a = b;
+
+b = c;
+
+c = temporario;
+
+
+
+console.log(`a = ${a} b = ${b} c = ${c}`);
